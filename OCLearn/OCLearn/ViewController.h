657a5dae6df8a0a9f7d2004be0cdf5c9ab9f260c
qq573092608 <1466081601@qq.com>
2017-08-04 19:09:50 +0800

    OC学习
    OC复习

---
+++ OCLearn/OCLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCLearn
+//
+//  Created by 黄小刚 on 2017/8/4.
+//  Copyright © 2017年 黄小刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCLearn/OCLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCLearn
+//
+//  Created by 黄小刚 on 2017/8/4.
+//  Copyright © 2017年 黄小刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
